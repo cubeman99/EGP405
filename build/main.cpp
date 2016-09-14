22ba13f53dee3b3ab3e7f18c544164142c0feb1e
Jordan <david.jordan@champlain.edu>
2016-09-14 10:46:35 -0400

    Initial work on networked menu assignment

--- build/main.cpp
+++ build/main.cpp
@@ -17,8 +17,144 @@ using namespace RakNet;
 
 enum GameMessages
 {
-	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1
+	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1,
+
+	ID_MENU_OPTION, // Client sends a chosen option ID to the server.
+	ID_MENU_DISPLAY_TEXT, // Server sends text to client to display a menu.
 };
+
+//
+//
+//class Menu
+//{
+//public:
+//
+//	Menu();
+//
+//	void AddOption(const std::string& option);
+//
+//private:
+//
+//	std::vector<std::string> m_options;
+//
+//};
+
+
+//Menu::Menu();
+
+
+void RunClient()
+{
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet *packet;
+
+	// Prompt for the IP address to connect to
+	char str[512];
+	printf("Enter server IP or hit enter for 127.0.0.1\n");
+	gets_s(str, sizeof(str));
+	if (str[0] == 0)
+	{
+		strcpy_s(str, "127.0.0.1");
+	}
+
+	// Connect to the server.
+	printf("Starting the client.\n");
+	peer->Connect(str, SERVER_PORT, 0, 0);
+
+
+	bool done = false;
+	bool anyErrors = false;
+
+	// 1. Receive a menu text.
+	// 2. Wait for user input for chosen option.
+
+	while (!done)
+	{
+		// Receive incoming packets.
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("We have been disconnected.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_CONNECTION_LOST:
+				printf("Connection lost.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				done = true;
+				break;
+			}
+		}
+	}
+
+	// Wait for a menu text message.
+	done = false;
+	while (!done)
+	{
+		// Receive incoming packets.
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+
+			switch (packet->data[0])
+			{
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("We have been disconnected.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_CONNECTION_LOST:
+				printf("Connection lost.\n");
+				anyErrors = true;
+				done = true;
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				done = true;
+				break;
+			case ID_GAME_MESSAGE_1:
+			{
+				RakNet::RakString rs;
+				RakNet::BitStream bsIn(packet->data, packet->length, false);
+				bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+				bsIn.Read(rs);
+				printf("%s\n", rs.C_String());
+				break;
+			}
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
+}
+
+void RunServer()
+{
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	RakNet::Packet *packet;
+
+	printf("Starting the server.\n");
+	// We need to let the server accept incoming connections from the clients
+	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+
+}
+
 
 int main(void)
 {
@@ -35,12 +171,14 @@ int main(void)
 	{
 		RakNet::SocketDescriptor sd;
 		peer->Startup(1, &sd, 1);
-		isServer = false;
+
+		RunServer();
 	}
 	else {
 		RakNet::SocketDescriptor sd(SERVER_PORT, 0);
 		peer->Startup(MAX_CLIENTS, &sd, 1);
-		isServer = true;
+
+		RunClient();
 	}
 
 	if (isServer)
