f91b6099d83283f827e8c87b45776e12a007a508
Jordan <david.jordan@CHAMPLAIN.EDU>
2016-09-26 13:07:43 -0400

    Release.bat is now working

--- Release.bat
+++ Release.bat
@@ -1,13 +1,13 @@
 
 mkdir Release
-mkdir Release/Executable
-mkdir Release/Source
+mkdir Release\Executable
+mkdir Release\Source
 
-copy build/GameNetworking.sln Release/Source
-copy build/GameNetworking.vcxproj Release/Source
-copy build/GameNetworking.vcxproj.filters Release/Source
-copy build/*.h Release/Source
-copy build/*.cpp Release/Source
+copy build\GameNetworking.sln Release\Source
+copy build\GameNetworking.vcxproj Release\Source
+copy build\GameNetworking.vcxproj.filters Release\Source
+copy build\*.h Release\Source
+copy build\*.cpp Release\Source
 
-copy build/Debug/GameNetworking.exe Release/Executable/GameNetworking.exe
+copy build\Debug\GameNetworking.exe Release\Executable\GameNetworking.exe
 
